a91c78ec58dcf56290b1c70e7d98d806f229eeb9
Leo LHUILLIER <leo.l-huillier@epitech.eu>
2023-09-26 18:15:00 +0200

    feat: add a start of navigation between basic pages

---
+++ web/src/pages/GetStarted.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../app/App.css';
+import { Text, VStack } from '@chakra-ui/react';
+import { Taskbar } from '../component/Taskbar';
+
+
+function Title() {
+    // faut trouver comment ajuster vrm la position le mettre ds un VStack c guez en plus jpe pas baisser le text
+    return (
+        <VStack spacing="0px">
+            <Text color="black" fontSize={{ base: '50px' }} >page get started to make</Text>
+        </VStack>
+    )
+}
+
+export const GetStarted = (): JSX.Element => {
+
+
+
+    return <div style={{
+        backgroundColor: "white", backgroundRepeat: "no-repeat", backgroundSize: "cover",
+        height: 930, width: 1905
+    }}>
+        <Taskbar></Taskbar>
+        <Title></Title>
+
+    </div>
+}
