076e111c0f984410f129b679aea712074859ea94
pablal levy <pablo.levy@epitech.eu>
2023-10-01 20:03:51 +0200

    feat: add base function for user and rename user directory

--- backend/src/authentication/auth.controller.ts
+++ backend/src/authentication/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
