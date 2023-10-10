c2cfa9ba9a5cce920d580ef171725123713982fc
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-10-10 07:26:51 +0200

    feat: request getNewReleases for Spotify Service

--- backend/src/services/spotify/spotify.controller.ts
+++ backend/src/services/spotify/spotify.controller.ts
@@ -55,4 +55,38 @@ export class SpotifyController {
     return res.status(200).send(result);
   }
 
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        country: {
+          type:'string'
+        },
+        limit: {
+          type:'number'
+        },
+        offset: {
+          type:'number'
+        }
+      }
+    }
+  })
+
+  @ApiOkResponse({
+    description: 'success',
+    type: String,
+    status: 200,
+  })
+
+  @Post('getNewReleases')
+  async getNewReleases(
+    @Res() res: Response,
+    @Body('country') country: string,
+    @Body('limit') limit: number,
+    @Body('offset') offset: number,
+    ): Promise<Response> {
+    const result = await this.SpotifyService.getNewReleases(country, limit, offset);
+    return res.status(200).send(result);
+  }
+
 }
--- backend/src/services/spotify/spotify.service.ts
+++ backend/src/services/spotify/spotify.service.ts
@@ -47,4 +47,19 @@ export class SpotifyService {
     });
   };
 
+  // https://api.spotify.com/v1/browse/new-releases
+
+  async getNewReleases(country: string, limit: number, offset: number): Promise<any> {
+    const access_token = await this.postToken();
+    const url = `https://api.spotify.com/v1/browse/new-releases?country=${country}&limit=${limit}&offset=${offset}`;
+
+    return await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    }).then((response: any) => {
+      return response.data;
+    });
+  };
+
 }
