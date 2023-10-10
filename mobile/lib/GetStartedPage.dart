7544e3f1ccc8ff2e14f7f6bf22d663d11c70f67b
laetitia <laetitia.bousch@epitech.eu>
2023-10-11 01:14:28 +0200

    feat: communication with Github OAUth2.0 mobile

--- mobile/lib/GetStartedPage.dart
+++ mobile/lib/GetStartedPage.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:mobile/SignUpPage.dart';
 import 'package:mobile/LoginPage.dart';
 import 'package:mobile/GetStartedEditorPage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'package:mobile/HomePage.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/Globals.dart' as globals;
+import 'dart:convert';
 
 class GetStartedPage extends StatefulWidget {
     final String title;
@@ -24,20 +29,99 @@ class _GetStartedPageState extends State<GetStartedPage> {
         'client_id=$clientIdGithub&'
         'scope=user';
 
-    try {
       final result = await FlutterWebAuth2.authenticate(
         url: authUrl,
         callbackUrlScheme: 'area',
       );
 
       print('Authentification réussie : $result');
-      Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => GetStartedEditorPage(title: 'GetStartedEditorPage')),
+
+      String? codeParam = Uri.parse(result).queryParameters['code'];
+
+      final response = await http.post(
+          Uri.parse('https://github.com/login/oauth/access_token'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({
+            'client_id': clientIdGithub,
+            'client_secret': 'c25bb753d702882f8634068356cabf8ce4c4ef8a',
+            'code': codeParam,
+          }),
       );
-    } catch (e) {
-      print('Erreur d\'authentification : $e');
-    }
+
+        if (response.statusCode == 200) { 
+      print('Dorian fils de papa');
+          List<String> parts = response.body.split('&');
+          String accessToken = parts[0].split('=')[1];
+
+          final userResponse = await http.get(
+              Uri.parse('https://api.github.com/user'),
+              headers: <String, String>{
+                  'Authorization': 'Bearer $accessToken',
+              },
+          );
+
+          if (userResponse.statusCode == 200) {
+              print('Dorian fils de maman');
+              final userData = jsonDecode(userResponse.body);
+              final username = userData['login'];
+              final email = userData['email'];
+
+              final response = await http.post(
+                  Uri.parse('http://'+globals.IPpc+':3000/auth/signup'),
+                  headers: {
+                      "Access-Control-Allow-Origin": "*",
+                  },
+                  body: {
+                      'mail': "789465132@epitech.eu",
+                      'username': username,
+                      'password': "password",
+                  },
+                );
+                print(response.statusCode);
+                if (response.statusCode == 200) {
+                    print('Dorian fils de PABLO');
+                    Fluttertoast.showToast(
+                        msg: "Enregistrement réussi !",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: Colors.green,
+                        textColor: Colors.white,
+                    );
+                } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text('Echec'),
+                        ),
+                    );
+                }
+                final responseLogin = await http.post(
+                    Uri.parse('http://'+globals.IPpc+':3000/auth/signin'),
+                    body: {
+                        'mail': "789465132@epitech.eu",
+                        'password': "password",
+                    },
+                );
+                if (responseLogin.statusCode == 200) {
+                    print('Dorian fils de TOM');
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text('Echec'),
+                        ),
+                    );
+                }
+            } else {
+                print('Erreur lors de la récupération des données de l\'utilisateur : ${userResponse.reasonPhrase}');
+            }
+        } else {
+          print('Erreur lors de l\'obtention du jeton d\'accès : ${response.reasonPhrase}');
+        }
   }
 
     @override
