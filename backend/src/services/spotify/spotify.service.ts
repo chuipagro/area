d6176b05fda50c48f1d55d09d1925fe143dc9057
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-10-10 06:49:44 +0200

    edit: change request postToken from Spotify Service into a more readable one

--- backend/src/services/spotify/spotify.service.ts
+++ backend/src/services/spotify/spotify.service.ts
@@ -19,63 +19,18 @@ export class SpotifyService {
     }
   }
 
-
-  //const getAuth = async () => {
-  //  try{
-  //    //make post request to SPOTIFY API for access token, sending relavent info
-  //    const token_url = 'https://accounts.spotify.com/api/token';
-  //    const data = qs.stringify({'grant_type':'client_credentials'});
-  //
-  //    const response = await axios.post(token_url, data, {
-  //      headers: { 
-  //        'Authorization': `Basic ${auth_token}`,
-  //        'Content-Type': 'application/x-www-form-urlencoded' 
-  //      }
-  //    })
-  //    //return access token
-  //    return response.data.access_token;
-  //    //console.log(response.data.access_token);   
-  //  }catch(error){
-  //    //on fail, log the error in console
-  //    console.log(error);
-  //  }
-  //}
-
   async postToken(): Promise<any> {
+    const url = `https://accounts.spotify.com/api/token`;
+    const auth_token = Buffer.from(`${this.clientID}:${this.clientSECRET}`, 'utf-8').toString('base64');
 
-    try{
-      //make post request to SPOTIFY API for access token, sending relavent info
-      const token_url = 'https://accounts.spotify.com/api/token';
-      const auth_token = Buffer.from(`${this.clientID}:${this.clientSECRET}`, 'utf-8').toString('base64');
-      //const data = qs.stringify({'grant_type':'client_credentials'});
-  
-      const response = await axios.post(token_url, {'grant_type':'client_credentials'}, {
-        headers: {
-          'Authorization': `Basic ${auth_token}`,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      //return access token
-      console.log(response.data.access_token);
+    return await axios.post(url, {'grant_type':'client_credentials'}, {
+      headers: {
+        'Authorization': `Basic ${auth_token}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then((response: any) => {
       return response.data.access_token;
-    }catch(error){
-      //on fail, log the error in console
-      console.log(error);
-    }
-
-
-    //console.log(Buffer.from((this.clientID + ':' + this.clientSECRET), 'utf8').toString('base64'));
-    //const url = `https://accounts.spotify.com/api/token`;
-    ////const data = qs.stringify({'grant_type':'client_credentials'});
-//
-    //return await axios.post(url, {'grant_type':'client_credentials'}, {
-    //        headers: {
-    //          'Authorization': `Basic ${this.clientSECRET}`,
-    //          'Content-Type': 'application/x-www-form-urlencoded'
-    //        }
-    //      }).then((response: any) => {
-    //  return response.data;
-    //});
+    });
   }
 
 }
