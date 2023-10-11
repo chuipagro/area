5f666e5b12a1f76c0de9e9724fd58f08191bc7b7
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-10-11 12:02:32 +0200

    feat: schedule for area

--- backend/src/cronjobs/cronjobs.service.ts
+++ backend/src/cronjobs/cronjobs.service.ts
@@ -1,13 +1,58 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { AreaSchema, ActionSchema } from '../models/area.model';
 
 @Injectable()
 export class CronjobsService {
   private readonly logger = new Logger(CronjobsService.name);
+  private readonly AreaSchema = [
+    {
+        title: "First Area",
+        active: true,
+        createdBy: "SteciFatiguant",
+        action: {
+          type: 0,
+          service: 0,
+        },
+        reaction: {
+          type: 0,
+          service: 0,
+        }
+    },
+    {
+        title: "string",
+        active: false,
+        createdBy: "string",
+        action: {
+          type: 0,
+          service: 0,
+        },
+        reaction: {
+          type: 0,
+          service: 0,
+        }
+    },
+    {
+        title: "string",
+        active: false,
+        createdBy: "string",
+        action: {
+          type: 0,
+          service: 0,
+        },
+        reaction: {
+          type: 0,
+          service: 0,
+        }
+    },
+  ];
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   handleCron() {
-
+    for (var area of this.AreaSchema) {
+        if (area["active"])
+            console.log(area); // prints values: 10, 20, 30, 40
+    }
     console.log('Called every minute');
   }
 }
