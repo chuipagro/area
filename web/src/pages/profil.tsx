c2948e96d4c9dff67edc61a19a8d9d4a7046c762
Leo LHUILLIER <leo.l-huillier@epitech.eu>
2023-11-05 20:25:30 +0100

    feat: add doc to profile

--- web/src/pages/profil.tsx
+++ web/src/pages/profil.tsx
@@ -16,7 +16,9 @@ function Title() {
     )
 }
 
-
+/**
+ * interface for the input
+ */
 interface InputProps {
     setValue: (value: string) => void;
     placeHolder: string;
@@ -25,6 +27,16 @@ interface InputProps {
     margin: string;
 }
 
+
+/**
+ * This function display an input
+ * @param setValue
+ * @param placeHolder
+ * @param type
+ * @param color
+ * @param margin
+ * @returns Input
+ */
 export function InputText({ setValue, placeHolder, type, color, margin }: InputProps): JSX.Element {
 
     const inputStyle = {
@@ -47,7 +59,6 @@ export function InputText({ setValue, placeHolder, type, color, margin }: InputP
 
 /**
  * This function display a page with the title and the taskbar
- * the function is to be completed
  */
 export const Profile = (): JSX.Element => {
 
@@ -109,6 +120,9 @@ export const Profile = (): JSX.Element => {
         }
     }, [jsonDataUser]);
 
+    /**
+     * this function fetch the json data from the api
+     */
     const fetchJsonDataOA2 = async () => {
         try {
             console.log('Fetching JSON data...');
@@ -135,6 +149,9 @@ export const Profile = (): JSX.Element => {
         fetchJsonDataOA2();
     }, []);
 
+    /**
+     * this function call the api to update the mail of a user
+     */
     const updateMail = async () => {
         try {
             console.log("email=" + email)
@@ -158,6 +175,9 @@ export const Profile = (): JSX.Element => {
         }
     };
 
+    /**
+     * this function call the api to update the username of a user
+     */
     const updateUsername = async () => {
         try {
 
@@ -186,6 +206,9 @@ export const Profile = (): JSX.Element => {
         }
     };
 
+    /**
+     * this function call the api to update the password of a user
+     */
     const updatePassword = async () => {
         try {
 
@@ -221,6 +244,9 @@ export const Profile = (): JSX.Element => {
     };
 
 
+    /**
+     * this function display a box with the name of the oauth2 connection
+     */
     function DisplayOauthNames() {
         const row = [];
 
@@ -243,6 +269,7 @@ export const Profile = (): JSX.Element => {
                 console.log(oauthNames[i])
                 row.push(
                     <Box p={5}
+                        // onClick=()
                         shadow='md'
                         borderRadius={30}
                         borderWidth='1px'
@@ -256,7 +283,6 @@ export const Profile = (): JSX.Element => {
             }
         }
         return (
-
             <VStack marginLeft={"-350px"} marginTop={"5%"} spacing="0px">
                 <Heading marginLeft={"-160px"} size="lg" color="black" marginTop={30} >Your OAuth2 connections</Heading>
                 {row}
@@ -264,7 +290,9 @@ export const Profile = (): JSX.Element => {
         );
     }
 
-
+    /**
+     * this function display a set of inputs for the user to modify his credencials
+     */
     function Account() {
 
         const inputStyle = {
@@ -344,6 +372,9 @@ export const Profile = (): JSX.Element => {
         );
     }
 
+    /**
+     * this function display the page
+     */
     function Display() {
         return (
             <div>
