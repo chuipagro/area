24cee86b8098a2a9b749c43f49908af919053a04
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-11-05 04:14:51 +0100

    feat: minecraft service reaction for new connection, disconnection and if new versions add on the server

--- backend/src/services/minecraft/minecraft.service.ts
+++ backend/src/services/minecraft/minecraft.service.ts
@@ -38,4 +38,87 @@ export class MinecraftService {
     });
   }
 
+  async waitForNewConnection(serverIP: string) : Promise<Object | null> {
+    const numberPlayers = await this.getNumberConnectedPlayers(serverIP.toString());
+    return new Promise<Object | null>(async (resolve, reject) => {
+      const cronGestion = new CronGestion();
+      const timezone = "Europe/Paris";
+
+      const checkForNewConnection = () => {
+        const dateAtCreation = new Date().toLocaleDateString();
+        const timeAtCreation = new Date().toLocaleTimeString();
+        const cronTimer = cronGestion.timerToCron("M1", timeAtCreation, dateAtCreation);
+
+        const job = cron.schedule(cronTimer, async () => {
+          const currentNumberPlayers = await this.getNumberConnectedPlayers(serverIP.toString());
+          if (numberPlayers < currentNumberPlayers) {
+            const numberNewPlayers = numberPlayers.online - currentNumberPlayers.online
+            resolve(numberNewPlayers);
+          }
+          return null;
+        }, {
+          scheduled: true,
+          timezone,
+        });
+        job.start();
+      }
+      checkForNewConnection();
+    });
+  }
+
+  async waitForNewDisconnection(serverIP: string) : Promise<Object | null> {
+    const numberPlayers = await this.getNumberConnectedPlayers(serverIP.toString());
+    return new Promise<Object | null>(async (resolve, reject) => {
+      const cronGestion = new CronGestion();
+      const timezone = "Europe/Paris";
+
+      const checkForNewDisconnection = () => {
+        const dateAtCreation = new Date().toLocaleDateString();
+        const timeAtCreation = new Date().toLocaleTimeString();
+        const cronTimer = cronGestion.timerToCron("M1", timeAtCreation, dateAtCreation);
+
+        const job = cron.schedule(cronTimer, async () => {
+          const currentNumberPlayers = await this.getNumberConnectedPlayers(serverIP.toString());
+          if (numberPlayers > currentNumberPlayers) {
+            const numberNewPlayers = currentNumberPlayers.online - numberPlayers.online
+            resolve(numberNewPlayers);
+          }
+          return null;
+        }, {
+          scheduled: true,
+          timezone,
+        });
+        job.start();
+      }
+      checkForNewDisconnection();
+    });
+  }
+
+  async waitForNewVersion(serverIP: string) : Promise<Object | null> {
+    const serverVersions = await this.getServerAvailaibleVersion(serverIP.toString());
+    return new Promise<Object | null>(async (resolve, reject) => {
+      const cronGestion = new CronGestion();
+      const timezone = "Europe/Paris";
+
+      const checkForNewVersion = () => {
+        const dateAtCreation = new Date().toLocaleDateString();
+        const timeAtCreation = new Date().toLocaleTimeString();
+        const cronTimer = cronGestion.timerToCron("H6", timeAtCreation, dateAtCreation);
+
+        const job = cron.schedule(cronTimer, async () => {
+          const currentServerVersions = await this.getServerAvailaibleVersion(serverIP.toString());
+          if (serverVersions != currentServerVersions) {
+            resolve(currentServerVersions);
+          }
+          return null;
+        }, {
+          scheduled: true,
+          timezone,
+        });
+        job.start();
+      }
+      checkForNewVersion();
+    });
+  }
+
 }
